fbd0eb03badb97b376164a46dcec7ab0f50e00ad
EmanisnamE <emanisnamebackwards@gmail.com>
2021-10-31 18:04:21 -0400

    added car rotation

--- Assets/scripts/car_movement.cs
+++ Assets/scripts/car_movement.cs
@@ -6,21 +6,24 @@ public class car_movement : MonoBehaviour
 {
     public float speed = 1.0f;
     public CharacterController controller;
+    public Transform carbody;
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 newPosition = transform.position;
+        
+
+        transform.position = newPosition;
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(x, z, 0);
+        Vector3 movement = transform.up * z;
         movement = movement.normalized * speed * Time.deltaTime;
+        newPosition = newPosition + movement;
 
-        //Vector3 newPosition = transform.position;
-        //newPosition = newPosition + movement;
-
-        //transform.position = newPosition;
         controller.Move(movement);
-        
+        carbody.Rotate(Vector3.back * x);
     }
 }
